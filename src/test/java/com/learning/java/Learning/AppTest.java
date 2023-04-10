ce94e74011f40387223c3d6638f024fc2cadcbe9
aisshaik <82267574+aisshaik@users.noreply.github.com>
2023-04-10 10:01:28 +0530

    Update AppTest.java

--- src/test/java/com/learning/java/Learning/AppTest.java
+++ src/test/java/com/learning/java/Learning/AppTest.java
@@ -3,6 +3,8 @@ package com.learning.java.Learning;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
 
 /**
  * Unit test for simple App.
